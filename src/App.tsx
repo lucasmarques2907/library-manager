041099a49ad94565387653d3c6822440a2118d79
lucasmarques2907 <lucasredra@gmail.com>
2026-07-05 15:13:24 -0300

    feat: CRUD operations

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,137 @@
-import { useState } from 'react'
+import { useState, type ChangeEvent } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  interface Book {
+    id: string;
+    title: string;
+    author: string;
+  }
+
+  const [books, setBooks] = useState<Book[]>([]);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [editingId, setEditingId] = useState("");
+
+  function handleSetTitle(e: ChangeEvent<HTMLInputElement, HTMLInputElement>) {
+    setTitle(e.target.value);
+  }
+
+  function handleSetAuthor(e: ChangeEvent<HTMLInputElement, HTMLInputElement>) {
+    setAuthor(e.target.value);
+  }
+
+  function handleAddBook() {
+    if (!title || !author) return;
+
+    setBooks([
+      ...books,
+      {
+        id: crypto.randomUUID(),
+        title: title,
+        author: author,
+      },
+    ]);
+
+    setTitle("");
+    setAuthor("");
+  }
+
+  function handleDeleteBook(id: string) {
+    setBooks(books.filter((book) => book.id !== id));
+
+    if (id === editingId) {
+      setTitle("");
+      setAuthor("");
+      setEditingId("");
+    }
+  }
+
+  function handleUpdateBook() {
+    if (!title || !author) return;
+
+    setBooks(
+      books.map((book) =>
+        book.id === editingId ? { ...book, title, author } : book,
+      ),
+    );
+
+    setTitle("");
+    setAuthor("");
+    setEditingId("");
+  }
+
+  function handleEditBook(id: string) {
+    const bookToEdit: Book | undefined = books.find((book) => book.id === id);
+    if (!bookToEdit) return;
+
+    setTitle(bookToEdit.title);
+    setAuthor(bookToEdit.author);
+    setEditingId(bookToEdit.id);
+  }
 
   return (
     <div>
-      <h1 className='text-red-400'>Hello World</h1>
+      <form className="flex gap-x-3">
+        <div className="flex flex-col max-w-3xs gap-y-1">
+          <label>Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => handleSetTitle(e)}
+            className="bg-white rounded-md px-2 py-1"
+          />
+        </div>
+        <div className="flex flex-col max-w-3xs gap-y-1">
+          <label>Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => handleSetAuthor(e)}
+            className="bg-white rounded-md px-2 py-1"
+          />
+        </div>
+        {editingId ? (
+          <button
+            type="button"
+            onClick={handleUpdateBook}
+            className="bg-yellow-700 text-yellow-100 h-fit px-4 py-1 rounded-md font-semibold self-end cursor-pointer"
+          >
+            Update
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={handleAddBook}
+            className="bg-green-700 text-green-100 h-fit px-4 py-1 rounded-md font-semibold self-end cursor-pointer"
+          >
+            Add
+          </button>
+        )}
+      </form>
+
+      <div>
+        {books.map((book, i) => (
+          <div key={i} className="flex gap-x-3">
+            <h1 className="text-white">
+              {book.title} • {book.author}
+            </h1>
+            <button
+              onClick={() => handleDeleteBook(book.id)}
+              className="bg-red-700 text-red-100 h-fit px-4 py-1 rounded-md font-semibold self-end cursor-pointer"
+            >
+              Delete
+            </button>
+            <button
+              onClick={() => handleEditBook(book.id)}
+              className="bg-yellow-700 text-yellow-100 h-fit px-4 py-1 rounded-md font-semibold self-end cursor-pointer"
+            >
+              Edit
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
