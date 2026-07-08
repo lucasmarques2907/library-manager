187a8e40aa9f0043722bffe44eb66ae4e5487362
lucasmarques2907 <lucasredra@gmail.com>
2026-07-08 13:55:36 -0300

    refactor: redesigning element structure and styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,27 @@ function App() {
   }
 
   const [books, setBooks] = useState<Book[]>([]);
+
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
+
   const [editingId, setEditingId] = useState("");
+  const [editingTitle, setEditingTitle] = useState("");
+  const [editingAuthor, setEditingAuthor] = useState("");
 
   function handleSetTitle(e: ChangeEvent<HTMLInputElement, HTMLInputElement>) {
     setTitle(e.target.value);
   }
 
-  function handleSetAuthor(e: ChangeEvent<HTMLInputElement, HTMLInputElement>) {
+  function handleSetAuthor(
+    e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
+  ) {
     setAuthor(e.target.value);
   }
 
-  function handleAddBook() {
+  function handleAddBook(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+
     if (!title || !author) return;
 
     setBooks([
@@ -34,130 +42,210 @@ function App() {
 
     setTitle("");
     setAuthor("");
+
+    (document.activeElement as HTMLElement)?.blur();
+  }
+
+  function handleSetEditingBook(id: string) {
+    const bookToEdit: Book | undefined = books.find((book) => book.id === id);
+    if (!bookToEdit) return;
+
+    setEditingId(bookToEdit.id);
+    setEditingTitle(bookToEdit.title);
+    setEditingAuthor(bookToEdit.author);
+  }
+
+  function handleSetEditingTitle(e: ChangeEvent<HTMLInputElement>) {
+    setEditingTitle(e.target.value);
+  }
+
+  function handleSetEditingAuthor(e: ChangeEvent<HTMLInputElement>) {
+    setEditingAuthor(e.target.value);
+  }
+
+  function handleEditBook(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!editingTitle || !editingAuthor) return;
+
+    setBooks(
+      books.map((book) =>
+        book.id === editingId
+          ? { ...book, title: editingTitle, author: editingAuthor }
+          : book,
+      ),
+    );
+
+    setEditingId("");
+    setEditingTitle("");
+    setEditingAuthor("");
+  }
+
+  function cancelEditBook() {
+    setEditingId("");
+    setEditingTitle("");
+    setEditingAuthor("");
   }
 
   function handleDeleteBook(id: string) {
     setBooks(books.filter((book) => book.id !== id));
-
-    if (id === editingId) {
-      setTitle("");
-      setAuthor("");
-      setEditingId("");
-    }
-  }
-
-  function handleUpdateBook() {
-    if (!title || !author) return;
-
-    setBooks(
-      books.map((book) =>
-        book.id === editingId ? { ...book, title, author } : book,
-      ),
-    );
-
-    setTitle("");
-    setAuthor("");
-    setEditingId("");
-  }
-
-  function handleEditBook(id: string) {
-    const bookToEdit: Book | undefined = books.find((book) => book.id === id);
-    if (!bookToEdit) return;
-
-    setTitle(bookToEdit.title);
-    setAuthor(bookToEdit.author);
-    setEditingId(bookToEdit.id);
   }
 
   return (
-    <div className="grid grid-rows-[auto_1fr] h-dvh p-4 sm:p-8 max-w-7xl mx-auto gap-y-4 sm:gap-y-8">
-      <form className="flex flex-col sm:flex-row gap-2 sm:gap-x-2 justify-center items-stretch sm:items-end">
-        <div className="flex flex-col w-full sm:max-w-3xs gap-y-1">
-          <label>Title</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => handleSetTitle(e)}
-            className="bg-transparent border-white border-2 text-white rounded-md px-2 py-1 w-full"
-          />
+    <div className='bg-orange-200/30 h-screen w-screen'>
+      <div className='mx-auto max-w-3xl p-8 flex flex-col gap-y-4 h-full'>
+        <div>
+          <h1 className='text-4xl text-taupe-600'>Library Manager</h1>
+          <h2 className='italic text-taupe-500 text-base'>
+            A simple library application to manage your books
+          </h2>
         </div>
-        <div className="flex flex-col w-full sm:max-w-3xs gap-y-1">
-          <label>Author</label>
-          <input
-            type="text"
-            value={author}
-            onChange={(e) => handleSetAuthor(e)}
-            className="bg-transparent border-white border-2 text-white rounded-md px-2 py-1 w-full"
-          />
-        </div>
-        {editingId ? (
-          <button
-            type="button"
-            onClick={handleUpdateBook}
-            className="bg-yellow-700 text-yellow-200 h-fit px-4 py-1 rounded-md font-semibold cursor-pointer w-full sm:w-auto"
-          >
-            Update
-          </button>
-        ) : (
-          <button
-            type="button"
-            onClick={handleAddBook}
-            className="bg-green-700 text-green-200 h-fit px-4 py-1.5 rounded-md font-semibold cursor-pointer w-full sm:w-auto"
-          >
-            Add
-          </button>
-        )}
-      </form>
 
-      <div
-        className={
-          books.length === 0
-            ? "hidden"
-            : "flex flex-col gap-y-2 items-center min-h-0"
-        }
-      >
-        <h2 className="text-xl font-bold text-white">Book List</h2>
-        <table className="w-full max-w-md rounded-md bg-neutral-700 overflow-hidden flex flex-col">
-          <thead>
-            <tr className="grid grid-cols-[1fr_1fr_auto] w-full">
-              <th className="text-left text-white p-2">Title</th>
-              <th className="text-left text-white p-2">Author</th>
-              <th className="text-center text-white p-2">Actions</th>
-            </tr>
-          </thead>
-          <tbody className="h-[calc(100dvh-200px)] overflow-y-auto flex flex-col">
-            {books.map((book, i) => (
-              <tr
-                key={i}
-                className={
-                  "grid grid-cols-[1fr_1fr_auto] w-full" +
-                  (i % 2 === 0 ? " bg-white" : " bg-white/90")
-                }
-              >
-                <td className="text-ellipsis overflow-hidden max-w-full p-2">
-                  {book.title}
-                </td>
-                <td className="text-ellipsis overflow-hidden max-w-full p-2">
-                  {book.author}
-                </td>
-                <td className="flex gap-x-1 sm:gap-x-2 p-2 whitespace-nowrap">
-                  <button
-                    className="bg-yellow-700 text-yellow-100 h-fit px-2 sm:px-4 py-1 rounded-md font-semibold cursor-pointer text-sm sm:text-base"
-                    onClick={() => handleEditBook(book.id)}
+        <form
+          onSubmit={(e) => handleAddBook(e)}
+          className='grid grid-cols-1 md:grid-cols-[1fr_1fr_auto] gap-3 justify-center items-end bg-white/40 p-4 rounded-lg shadow-sm border border-taupe-400/40'
+        >
+          <div className='flex flex-col gap-y-0.5'>
+            <label
+              htmlFor='title_add'
+              className='text-xs font- tracking-widest text-taupe-500'
+            >
+              TITLE
+            </label>
+            <input
+              name='title'
+              id='title_add'
+              placeholder='The Lord of the Rings'
+              value={title}
+              onChange={(e) => handleSetTitle(e)}
+              className='text-sm text-taupe-600 bg-white rounded-md border border-taupe-400/40 px-3 py-2 placeholder:text-taupe-400 focus:outline-none focus:ring-2 focus:ring-amber-500'
+            />
+          </div>
+          <div className='flex flex-col gap-y-0.5'>
+            <label
+              htmlFor='author_add'
+              className='text-xs tracking-widest text-taupe-500'
+            >
+              AUTHOR
+            </label>
+            <input
+              name='author'
+              id='author_add'
+              placeholder='J. R. R. Tolkien'
+              value={author}
+              onChange={(e) => handleSetAuthor(e)}
+              className='text-sm text-taupe-600 bg-white rounded-md border border-taupe-400/40 px-3 py-2 placeholder:text-taupe-400  focus:outline-none focus:ring-2 focus:ring-amber-500'
+            />
+          </div>
+          <button
+            type='submit'
+            disabled={!title || !author}
+            className='bg-amber-800 text-white text-sm rounded-md h-9.5 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50'
+          >
+            Add book
+          </button>
+        </form>
+
+        <div
+          className={
+            books.length === 0
+              ? "hidden"
+              : "flex flex-col gap-y-2 flex-1 min-h-0"
+          }
+        >
+          <p className='text-taupe-500 text-base'>
+            {books.length} {books.length === 1 ? "book" : "books"} on the shelf
+          </p>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 py-2 overflow-y-auto mask-b-from-98% mask-t-from-98%'>
+            {books.map((book, i) =>
+              book.id === editingId ? (
+                <form
+                  onSubmit={handleEditBook}
+                  className='h-full flex flex-col gap-3 p-2 bg-white/30 shadow-sm rounded-lg justify-between'
+                >
+                  <div className='flex flex-col gap-3'>
+                    <input
+                      name='title'
+                      id='title_edit'
+                      placeholder='Title'
+                      value={editingTitle}
+                      onChange={(e) => handleSetEditingTitle(e)}
+                      className='text-sm text-taupe-600 bg-white rounded-md border border-taupe-400/40 px-3 py-2 placeholder:text-taupe-400 focus:outline-none focus:ring-2 focus:ring-amber-500'
+                    />
+                    <input
+                      name='author'
+                      id='author_edit'
+                      placeholder='Author'
+                      value={editingAuthor}
+                      onChange={(e) => handleSetEditingAuthor(e)}
+                      className='text-sm text-taupe-600 bg-white rounded-md border border-taupe-400/40 px-3 py-2 placeholder:text-taupe-400 focus:outline-none focus:ring-2 focus:ring-amber-500'
+                    />
+                  </div>
+
+                  <div className='grid grid-cols-[1fr_1fr] gap-x-2 align-bottom'>
+                    <button
+                      type='button'
+                      onClick={cancelEditBook}
+                      className='w-full text-amber-800 border border-amber-800 text-sm rounded-md h-9.5 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500 cursor-pointer'
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type='submit'
+                      disabled={!editingTitle || !editingAuthor}
+                      className='w-full bg-amber-800 text-white text-sm rounded-md h-9.5 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50'
+                    >
+                      Save
+                    </button>
+                  </div>
+                </form>
+              ) : (
+                <div
+                  key={i}
+                  className='flex flex-col gap-y-2 bg-white/30 shadow-sm rounded-lg'
+                >
+                  <div
+                    className={`h-20 rounded-t-lg relative ${i % 2 === 0 ? "bg-orange-900/70" : "bg-amber-900/70"}`}
                   >
-                    Edit
-                  </button>
-                  <button
-                    className="bg-red-700 text-red-100 h-fit px-2 sm:px-4 py-1 rounded-md font-semibold cursor-pointer text-sm sm:text-base"
-                    onClick={() => handleDeleteBook(book.id)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                    <div className='w-1 h-full bg-white/10 absolute left-3'></div>
+                    <p className='text-white/70 text-4xl absolute bottom-0 left-3'>
+                      {book.title[0].toUpperCase()}
+                    </p>
+                  </div>
+                  <div className='px-3'>
+                    <p
+                      title={book.title}
+                      className='text-taupe-600 text-base font-semibold leading-tight truncate'
+                    >
+                      {book.title}
+                    </p>
+                    <p
+                      title={book.author}
+                      className='text-taupe-500 text-sm italic leading-tight truncate'
+                    >
+                      by {book.author}
+                    </p>
+                  </div>
+                  <div className='w-full h-px bg-taupe-500/10'></div>
+                  <div className='flex gap-x-4 text-sm px-3 pb-2'>
+                    <button
+                      className='text-yellow-700 cursor-pointer h-fit leading-tight focus:outline-none focus:ring-2 focus:ring-amber-500'
+                      onClick={() => handleSetEditingBook(book.id)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className='text-orange-700 cursor-pointer h-fit leading-tight focus:outline-none focus:ring-2 focus:ring-amber-500'
+                      onClick={() => handleDeleteBook(book.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ),
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
 import App from './App.tsx'
+import '@fontsource-variable/eb-garamond'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
